50cc08715a09b332311ada86628ef15bbde950d3
Nandhika A <139376918+nandhika66@users.noreply.github.com>
2025-03-11 17:57:25 +0530

    Create items.js

---
+++ Smart Navigation/src/data/items.js
@@ -0,0 +1,47 @@
+import { image } from "framer-motion/client";
+
+export const items = [
+    {
+      id: "1",
+      name: "Dairy Milk Chocolate",
+      description: "A delicious and creamy milk chocolate, perfect for any time of the day.",
+      price: "50.00",
+      location: "Aisle 4, Rack 2, Shelve 3",
+      image:"/dairy_milk.jpeg",
+    },
+    {
+      id: "2",
+      name: "Surf Excel Detergent Powder",
+      description: "Powerful stain removal detergent that works in just one wash.",
+      price: "180.00",
+      location: "Aisle 4, Section 1",
+      image:"/surf_excel.jpeg",
+    },
+    {
+      id: "3",
+      name: "Tata Salt",
+      description: "Iodized salt to keep you healthy and strong, perfect for everyday cooking.",
+      price: "20.00",
+      location: "Aisle 1, Section 2",
+      image:"/tata_salt.jpg",
+    },
+    {
+      id: "4",
+      name: "Parle-G Biscuits",
+      description: "Classic glucose biscuits that are perfect for tea-time snacks.",
+      price: "10.00",
+      location: "Aisle 3, Section 4",
+      image:"/parleg.jpg",
+    },
+    {
+      id: "5",
+      name: "Fortune Sunflower Oil",
+      description: "Light and healthy sunflower oil for all your cooking needs.",
+      price: "200.00",
+      location: "Aisle 5, Section 2",
+      image:"/fortune.jpg",
+    }
+  
+  ];
+  
+  
